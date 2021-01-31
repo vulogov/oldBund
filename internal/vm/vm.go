d64b207a91a7801a6d02258ab6c3262c0459c22e
Vlafimir Ulogov <vladimir.ulogov@me.com>
2021-01-30 23:28:24 -0800

    chore: Some basic vm.Stack operation

--- internal/vm/vm.go
+++ internal/vm/vm.go
@@ -1,9 +1,22 @@
 package vm
 
 import (
+	"sync"
+
+	"github.com/gammazero/deque"
+
 	bcmd "github.com/vulogov/Bund/internal/cmd"
 )
 
 type VM struct {
-	cmd bcmd.CMD
+	name  string
+	lock  sync.RWMutex
+	cmd   bcmd.CMD
+	stack deque.Deque
+}
+
+func New(name string) *VM {
+	res := new(VM)
+	res.name = name
+	return res
 }
