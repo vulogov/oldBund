7565524aeefade6ff499d4ae80de07c4301210a0
Vlafimir Ulogov <vladimir.ulogov@me.com>
2021-01-30 23:29:00 -0800

    fix: Fixing some methods of VM

--- internal/vm/vm.go
+++ internal/vm/vm.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gammazero/deque"
 
 	bcmd "github.com/vulogov/Bund/internal/cmd"
+  "github.com/vulogov/Bund/internal/value"
+
 )
 
 type VM struct {
@@ -19,4 +21,24 @@ func New(name string) *VM {
 	res := new(VM)
 	res.name = name
 	return res
+}
+
+func (* v) Put(v *value.VALUE) int {
+  f.lock.Lock()
+  v.stack.PushBack(v)
+  n := v.stack.Len()
+  f.lock.Unlock()
+  return n
+}
+
+func (* v) Get() *value.VALUE {
+  var res *value.VALUE
+  f.lock.Lock()
+  if v.stack.Len() > 0 {
+    res = v.stack.PopBack()
+  } else {
+    res = nil
+  }
+  f.lock.Unlock()
+  return res
 }
